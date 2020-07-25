c031cee43cd9997cdeeb55096be7d4948cf3cc0c
Щипин Дмитрий <mrethorill@mail.ru>
2020-07-26 01:39:22 +0300

    Релиз помошника_12

--- homework.py
+++ homework.py
@@ -6,6 +6,7 @@ import logging
 
 from dotenv import load_dotenv
 
+logging.basicConfig(level=logging.DEBUG)
 load_dotenv()
 
 PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
@@ -17,16 +18,21 @@ bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
 
 def parse_homework_status(homework):
-    homework_name = homework['homework_name'].split('.')[0]
-    status_work = homework['status']
-    if status_work != 'approved':
+    homework_name = homework.get('homework_name').split('.')[0]
+    status_work = homework.get('status')
+    if status_work != 'rejected' and status_work != 'approved':
+        logging.error('Мы получили неверный ответ от Яндекса')
+    if status_work == 'rejected' and homework_name is not None:
         verdict = 'К сожалению в работе нашлись ошибки.'
-    else:
+        return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+    if status_work == 'approved' and homework_name is not None:
         verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
-    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+        return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
 def get_homework_statuses(current_timestamp):
+    if current_timestamp is None:
+        current_timestamp = int(time.time())
     headers = {
         'Authorization': f'OAuth {PRACTICUM_TOKEN}',
     }
@@ -35,7 +41,11 @@ def get_homework_statuses(current_timestamp):
     }
     homework_statuses = requests.get(URL, params=params,
                                      headers=headers)
-    return homework_statuses.json()
+    try:
+        return homework_statuses.json()
+    except Exception as error:
+        logging.info(f'Ошибка {error}')
+        logging.error('Что то не так с API')
 
 
 def send_message(message):
@@ -55,7 +65,6 @@ def main():
             time.sleep(1140)
 
         except Exception as e:
-            logging.basicConfig(level=logging.DEBUG)
             logging.debug(f'Ошибка: {e}')
             logging.error(u'Бот упал :(')
             time.sleep(10)
